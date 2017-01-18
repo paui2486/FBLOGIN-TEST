c94d6a4299f1bb21eb1ee9cf3a73e20a4aa04e03
paui <paui0989902413@mail.com>
2017-01-18 15:41:01 +0800

    為啥編譯的過 但是一執行就掛

---
+++ app/src/main/java/com/example/sam/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.sam.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class MainActivity extends Activity {
+
+    //CallbackManager callbackManager; //FB登入專用變數一
+    //private AccessToken accessToken; //FB登入專用變數二
+    private CallbackManager callbackManager;
+    private TextView info;
+    private LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        //AppEventsLogger.activateApp(this);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User ID:  " +
+                        loginResult.getAccessToken().getUserId() + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
